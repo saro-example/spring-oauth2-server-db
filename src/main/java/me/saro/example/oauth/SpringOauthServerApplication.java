e808796ef0215acbdda9d1770384cef947a2dfaf
PARK Yong Seo <j@saro.me>
2019-01-14 00:01:45 +0900

    init

---
+++ src/main/java/me/saro/example/oauth/SpringOauthServerApplication.java
@@ -0,0 +1,14 @@
+package me.saro.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthServerApplication.class, args);
+	}
+
+}
+
