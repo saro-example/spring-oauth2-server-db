edf514c8b08a7139f872cc06a63a5064df726938
PARK Yong Seo <j@saro.me>
2019-01-17 02:31:57 +0900

    using the optional

--- src/main/java/db/oauth/repository/AccountRepository.java
+++ src/main/java/db/oauth/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package db.oauth.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +12,5 @@ import db.oauth.entity.Account;
 public interface AccountRepository extends CrudRepository<Account, Long>
 {
     @Query("SELECT a FROM account a WHERE ac = :ac")
-    Account findByAc(@Param("ac") String ac);
+    Optional<Account> findByAc(@Param("ac") String ac);
 }
