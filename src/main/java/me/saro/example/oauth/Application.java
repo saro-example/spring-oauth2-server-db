6e21f04cd2b434f80c7a1c811400220721130242
PARK Yong Seo <j@saro.me>
2019-01-21 02:29:56 +0900

    scan

--- src/main/java/me/saro/example/oauth/Application.java
+++ src/main/java/me/saro/example/oauth/Application.java
@@ -2,7 +2,9 @@ package me.saro.example.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"me.saro.example.oauth.service", "me.saro.example.oauth.configuration"})
 @SpringBootApplication
 public class Application {
 
