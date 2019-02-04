0fcb35b22879ba465aa5d268b1330927126d86d9
PARK Yong Seo <j@saro.me>
2019-02-04 16:31:01 +0900

    server config

---
+++ src/main/java/me/saro/example/oauth/configuration/ServerSecurityConfig.java
@@ -0,0 +1,39 @@
+package me.saro.example.oauth.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring().antMatchers("/");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+            .authorizeRequests()
+            .antMatchers("/**").authenticated();
+        
+        http
+            .csrf().disable();
+        
+        http
+            .formLogin()
+            .loginProcessingUrl("/login");
+        
+        http
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/");
+    }
+}
